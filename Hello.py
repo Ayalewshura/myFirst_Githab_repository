7f2a9293324422c6b3035644745467cd315ebbaf
Ayalewshura <ayalewkasa@gmail.com>
2024-12-29 14:42:35 +0100

    Update Hello.py
    I added doc string and my excitement to the file

--- Hello.py
+++ Hello.py
@@ -3,3 +3,7 @@ print("Hello Github")
 print("then")
 
 print("Hello Ayalew")
+
+'''
+This is first step to use Gih=thub and grow professionally and update my code and storethem on thebgithub repositoiry
+'''
